a0ae74e707742b15919a0d3f792f968ccd533999
Daniel <danielochoa@outlook.com>
2021-09-02 15:30:12 -0500

    Agregando spring boot JPA en el build.gradle

--- market/src/main/java/com/danicode/market/web/controller/SaludoController.java
+++ market/src/main/java/com/danicode/market/web/controller/SaludoController.java
@@ -1,4 +1,4 @@
-package com.danicode.market.controller;
+package com.danicode.market.web.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
