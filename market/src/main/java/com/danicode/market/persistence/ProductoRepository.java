0aca3f95533115762d0ddcb1ff5201579820b5a7
Daniel <danielochoa@outlook.com>
2021-09-07 10:14:20 -0500

    Corrigiendo mapeo de variables en ProductMapper | Agregando controlador ProductController

--- market/src/main/java/com/danicode/market/persistence/ProductoRepository.java
+++ market/src/main/java/com/danicode/market/persistence/ProductoRepository.java
@@ -26,37 +26,36 @@ public class ProductoRepository implements ProductRepository {
 	@Override
 	public List<Product> getAll() {
 		List<Producto> productos = (List<Producto>) this.productoCrudRepository.findAll();
-		return this.mapper.toProducts(productos);
+		return mapper.toProducts(productos);
 	}
 
 	@Override
 	public void delete(int productId) {
-		this.productoCrudRepository.deleteById(productId);
+		productoCrudRepository.deleteById(productId);
 	}
 
 	@Override
 	public Optional<List<Product>> getByCategory(int categoryId) {
 		List<Producto> productos = this.productoCrudRepository.findByIdCategoriaOrderByNombreAsc(categoryId);
-		return Optional.of(this.mapper.toProducts(productos));
+		return Optional.of(mapper.toProducts(productos));
 	}
 
 	@Override
 	public Optional<List<Product>> getScarseProducts(int quantity) {
-		Optional<List<Producto>> productos = this.productoCrudRepository.findByCantidadStockLessThanAndEstado(quantity,
+		Optional<List<Producto>> productos = productoCrudRepository.findByCantidadStockLessThanAndEstado(quantity,
 				true);
 		return productos.map(producto -> mapper.toProducts(producto));
 	}
 
 	@Override
 	public Optional<Product> getProduct(int productId) {
-		return this.productoCrudRepository.findById(productId).map(producto -> mapper.toProduct(producto));
+		return productoCrudRepository.findById(productId).map(producto -> mapper.toProduct(producto));
 
 	}
 
 	@Override
 	public Product save(Product product) {
-		Producto producto = this.mapper.toProducto(product);
-		return this.mapper.toProduct(this.productoCrudRepository.save(producto));
-
+		Producto producto = mapper.toProducto(product);
+		return mapper.toProduct(productoCrudRepository.save(producto));
 	}
 }
--- market/src/main/java/com/danicode/market/persistence/mapper/ProductMapper.java
+++ market/src/main/java/com/danicode/market/persistence/mapper/ProductMapper.java
@@ -13,14 +13,10 @@ import com.danicode.market.persistence.entity.Producto;
 @Mapper(componentModel = "spring", uses = { CategoryMapper.class })
 public interface ProductMapper {
 
-	@Mappings({ 
-		@Mapping(source = "id_producto", target = "productId"), 
-		@Mapping(source = "nombre", target = "name"),
-		@Mapping(source = "idCategoria", target = "categoryId"),
-		@Mapping(source = "precioVenta", target = "price"),
-		@Mapping(source = "cantidadStock", target = "price"), 
-		@Mapping(source = "estado", target = "active"),
-		@Mapping(source = "categoria", target = "category"), })
+	@Mappings({ @Mapping(source = "idProducto", target = "productId"), @Mapping(source = "nombre", target = "name"),
+			@Mapping(source = "idCategoria", target = "categoryId"), @Mapping(source = "precioVenta", target = "price"),
+			@Mapping(source = "cantidadStock", target = "stock"), @Mapping(source = "estado", target = "active"),
+			@Mapping(source = "categoria", target = "category"), })
 	Product toProduct(Producto producto);
 
 	List<Product> toProducts(List<Producto> productos);
