fa2ca154ca9f44a0b4ddcb4ed00a7d11c3a5cf44
Daniel <danielochoa@outlook.com>
2021-09-09 10:47:01 -0500

    Corrigiendo clase Purchase y PurchaseMapper (atributo items) | Agregando dependencias Swagger2 y Swagger-ui para documentación de API (ProductController)

--- market/src/main/java/com/danicode/market/web/controller/ProductController.java
+++ market/src/main/java/com/danicode/market/web/controller/ProductController.java
@@ -5,7 +5,10 @@ package com.danicode.market.web.controller;
 import java.util.List;
 import java.util.Optional;
 
-import org.apache.coyote.Response;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -28,19 +31,31 @@ public class ProductController {
     private ProductService productService;
 
     @GetMapping("/all")
+    @ApiOperation("Get all supermarket products")
+    @ApiResponse(code = 200, message = "OK")
     public ResponseEntity<List<Product>> getAll() {
         return new ResponseEntity<>(this.productService.getAll(), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Product> getProduct(@PathVariable("id") int productId) {
+    @ApiOperation("Search a product with ID")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "OK"),
+            @ApiResponse(code = 404, message = "Product not found"),
+    })
+    public ResponseEntity<Product> getProduct(@ApiParam(value = "ID of product", required = true, example = "7") @PathVariable("id") int productId) {
         return this.productService.getProduct(productId)
                 .map(product -> new ResponseEntity<>(product, HttpStatus.OK))
                 .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
     @GetMapping("/category/{categoryId}")
-    public ResponseEntity<List<Product>> getByCategory(@PathVariable("categoryId") int categoryId) {
+    @ApiOperation("Get all products according to a category")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "OK"),
+            @ApiResponse(code = 404, message = "Products not found"),
+    })
+    public ResponseEntity<List<Product>> getByCategory(@ApiParam(value = "ID of category", required = true, example = "3") @PathVariable("categoryId") int categoryId) {
         return this.productService.getByCategory(categoryId)
                 .map(products -> new ResponseEntity<>(products, HttpStatus.OK))
                 .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
