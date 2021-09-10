1dc7a087a8b8b8d25f3ad302297a7392e70a2437
Daniel <danielochoa@outlook.com>
2021-09-10 10:06:39 -0500

    Creando clase JWTUtil donde habrá una función que generará un JWT con las credenciales creadas anteriormente y que tendrpá una duración de 10 horas

---
+++ market/src/main/java/com/danicode/market/web/security/JWTUtil.java
@@ -0,0 +1,22 @@
+package com.danicode.market.web.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Date;
+
+public class JWTUtil {
+
+    private static final String KEY = "danicode";
+
+    public String generateToken(UserDetails userDetails) {
+        // setIssuedAt > Fecha de creacion del token
+        // setExpiration > 10 horas
+        return Jwts.builder().setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
+                .signWith(SignatureAlgorithm.HS256, KEY)
+                .compact();
+    }
+}
