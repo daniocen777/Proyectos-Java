831cdeb978747863bbe3d4031f48fd198ee3bca3
Daniel <danielochoa@outlook.com>
2021-09-13 09:02:34 -0500

    Sobreescribiendo método configure para permitir solo a las peticiones que tengan al final /authenticate | Sobreescribiendo método AuthenticationManager de Spring para usarlo en controlador AuthController

--- market/src/main/java/com/danicode/market/web/security/SecurityConfig.java
+++ market/src/main/java/com/danicode/market/web/security/SecurityConfig.java
@@ -2,7 +2,10 @@ package com.danicode.market.web.security;
 
 import com.danicode.market.domain.service.DanicodeUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -16,5 +19,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(danicodeUserDetailsService);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // PERMITIR Todas las peticiones que terminen en authenticate
+        // Las otras peticiones, necesitan autenticarse => anyRequest().authenticated()
+        http.csrf().disable().authorizeRequests()
+                .antMatchers("/**/authenticate").permitAll()
+                .anyRequest().authenticated();
+    }
+
+    // Incluir AuthenticationManager con anotación @Bean para inyectarlo y usarlo
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 }
