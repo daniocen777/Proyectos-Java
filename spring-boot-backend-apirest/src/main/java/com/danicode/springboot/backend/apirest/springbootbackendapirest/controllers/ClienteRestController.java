db7e291357b4500660e228e3ed8a310944dfa318
daniocen777 <danielochoa@outlook.com>
2021-10-16 09:00:44 -0500

    Agregando métodos CRUD al controlador

--- spring-boot-backend-apirest/src/main/java/com/danicode/springboot/backend/apirest/springbootbackendapirest/controllers/ClienteRestController.java
+++ spring-boot-backend-apirest/src/main/java/com/danicode/springboot/backend/apirest/springbootbackendapirest/controllers/ClienteRestController.java
@@ -3,10 +3,9 @@ package com.danicode.springboot.backend.apirest.springbootbackendapirest.control
 import com.danicode.springboot.backend.apirest.springbootbackendapirest.models.entity.Cliente;
 import com.danicode.springboot.backend.apirest.springbootbackendapirest.models.services.IClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
 
 import java.util.List;
 
@@ -17,8 +16,35 @@ public class ClienteRestController {
     @Autowired
     private IClienteService clienteService;
 
-    @GetMapping("")
+    @GetMapping({"/", "", "/all"})
     public List<Cliente> index() {
-        return clienteService.findAll();
+        return this.clienteService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Cliente show(@PathVariable Long id) {
+        return this.clienteService.findById(id);
+    }
+
+    @PostMapping({"/", ""})
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cliente store(@RequestBody Cliente cliente) {
+        return this.clienteService.save(cliente);
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cliente update(@PathVariable Long id, @RequestBody Cliente cliente) {
+        Cliente clienteActual = this.clienteService.findById(id);
+        clienteActual.setApellido(cliente.getApellido());
+        clienteActual.setNombre(cliente.getNombre());
+        clienteActual.setEmail(cliente.getEmail());
+        return this.clienteService.save(clienteActual);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void update(@PathVariable Long id) {
+        this.clienteService.delete(id);
     }
 }
