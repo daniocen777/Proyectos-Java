b6900c13616a2c2b6af3fa9348e5060667a53aa0
daniocen777 <danielochoa@outlook.com>
2021-10-16 08:41:41 -0500

    Implementando métodos CRUD

--- spring-boot-backend-apirest/src/main/java/com/danicode/springboot/backend/apirest/springbootbackendapirest/models/services/ClienteServiceImpl.java
+++ spring-boot-backend-apirest/src/main/java/com/danicode/springboot/backend/apirest/springbootbackendapirest/models/services/ClienteServiceImpl.java
@@ -1,10 +1,12 @@
 package com.danicode.springboot.backend.apirest.springbootbackendapirest.models.services;
 
 import java.util.List;
+
 import com.danicode.springboot.backend.apirest.springbootbackendapirest.models.dao.IClienteDao;
 import com.danicode.springboot.backend.apirest.springbootbackendapirest.models.entity.Cliente;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class ClienteServiceImpl implements IClienteService {
@@ -13,7 +15,26 @@ public class ClienteServiceImpl implements IClienteService {
     private IClienteDao clienteDao; // transactional default
 
     @Override
+    @Transactional(readOnly = true)
     public List<Cliente> findAll() {
-        return (List<Cliente>) clienteDao.findAll();
+        return (List<Cliente>) this.clienteDao.findAll();
+    }
+
+    @Override
+    @Transactional
+    public Cliente save(Cliente cliente) {
+        return this.clienteDao.save(cliente);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Cliente findById(Long id) {
+        return this.clienteDao.findById(id).orElse(null);
+    }
+
+    @Override
+    @Transactional
+    public void delete(Long id) {
+        this.clienteDao.deleteById(id);
     }
 }
