7881676a43b9f03291870915f555a418791acb2f
daniocen777 <danielochoa@outlook.com>
2021-10-29 09:10:10 -0500

    Agregando método en el controlador para listar clientes paginados

--- spring-boot-backend-apirest/src/main/java/com/danicode/springboot/backend/apirest/springbootbackendapirest/controllers/ClienteRestController.java
+++ spring-boot-backend-apirest/src/main/java/com/danicode/springboot/backend/apirest/springbootbackendapirest/controllers/ClienteRestController.java
@@ -4,6 +4,9 @@ import com.danicode.springboot.backend.apirest.springbootbackendapirest.models.e
 import com.danicode.springboot.backend.apirest.springbootbackendapirest.models.services.IClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
@@ -25,6 +28,12 @@ public class ClienteRestController {
     @GetMapping({"/", "", "/all"})
     public List<Cliente> index() {
         return this.clienteService.findAll();
+    }
+
+    @GetMapping({"/page/{page}", "", "/all/page/{page}"})
+    public Page<Cliente> index(@PathVariable Integer page) {
+        Pageable pageable = PageRequest.of(page, 4);
+        return this.clienteService.findAll(pageable);
     }
 
     @GetMapping("/{id}")
