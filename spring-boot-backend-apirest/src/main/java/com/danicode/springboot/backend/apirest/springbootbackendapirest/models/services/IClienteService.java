391e002ef5b81aeba1133c7d85e22f6b77f9a42a
daniocen777 <danielochoa@outlook.com>
2021-10-29 09:09:13 -0500

    Agregando método para listar clientes paginados

--- spring-boot-backend-apirest/src/main/java/com/danicode/springboot/backend/apirest/springbootbackendapirest/models/services/IClienteService.java
+++ spring-boot-backend-apirest/src/main/java/com/danicode/springboot/backend/apirest/springbootbackendapirest/models/services/IClienteService.java
@@ -1,11 +1,15 @@
 package com.danicode.springboot.backend.apirest.springbootbackendapirest.models.services;
 
 import com.danicode.springboot.backend.apirest.springbootbackendapirest.models.entity.Cliente;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface IClienteService {
     public List<Cliente> findAll();
+
+    public Page<Cliente> findAll(Pageable pageable);
 
     public Cliente save(Cliente cliente);
 
