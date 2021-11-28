fdf27378fe79c363d937b1bc897251f26bf4d904
daniocen777 <danielochoa@outlook.com>
2021-11-28 09:06:01 -0500

    Agregando Query para listar las regiones

--- spring-boot-backend-apirest/src/main/java/com/danicode/springboot/backend/apirest/springbootbackendapirest/models/dao/IClienteDao.java
+++ spring-boot-backend-apirest/src/main/java/com/danicode/springboot/backend/apirest/springbootbackendapirest/models/dao/IClienteDao.java
@@ -1,8 +1,15 @@
 package com.danicode.springboot.backend.apirest.springbootbackendapirest.models.dao;
 
 import com.danicode.springboot.backend.apirest.springbootbackendapirest.models.entity.Cliente;
+import com.danicode.springboot.backend.apirest.springbootbackendapirest.models.entity.Region;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 // JpaRepository (hereda de PaginAndSortingRepository => Para usar pagination
 public interface IClienteDao extends JpaRepository<Cliente, Long> {
+    // Listado de regiones (solo necesito la lista, no generar el CRUD
+    @Query("from Region")
+    public List<Region> findAllRegiones();
 }
