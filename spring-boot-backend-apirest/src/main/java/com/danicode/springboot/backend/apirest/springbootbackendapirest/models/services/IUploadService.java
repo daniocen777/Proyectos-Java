de973d3700f468cffb04e1c2a5119834f5e37b54
daniocen777 <danielochoa@outlook.com>
2021-11-28 08:35:58 -0500

    Creando interface para manejar los archivos de foto

---
+++ spring-boot-backend-apirest/src/main/java/com/danicode/springboot/backend/apirest/springbootbackendapirest/models/services/IUploadService.java
@@ -0,0 +1,19 @@
+package com.danicode.springboot.backend.apirest.springbootbackendapirest.models.services;
+
+import org.springframework.core.io.Resource;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.file.Path;
+
+public interface IUploadService {
+
+    public Resource cargar(String nombreFoto) throws MalformedURLException;
+
+    public String guardarImage(MultipartFile archivo) throws IOException;
+
+    public boolean eliminar(String nombreFoto);
+
+    public Path getPath(String nombreFoto);
+}
