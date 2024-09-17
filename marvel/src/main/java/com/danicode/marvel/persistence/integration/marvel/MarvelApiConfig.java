6f00660e614fdf0c523c7a47c11bbc8404496dbb
daniocen777 <danielochoa@outlook.com>
2024-09-17 09:29:51 -0500

    Se creó la configuración para autenticarse a la api de marvel

--- marvel/src/main/java/com/danicode/marvel/persistence/integration/marvel/MarvelApiConfig.java
+++ marvel/src/main/java/com/danicode/marvel/persistence/integration/marvel/MarvelApiConfig.java
@@ -1,12 +1,22 @@
 package com.danicode.marvel.persistence.integration.marvel;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 public class MarvelApiConfig {
+
+    @Autowired
+    @Qualifier("md5Encoder")
+    private PasswordEncoder md5Encoder;
+
     private long timestamp = new Date(System.currentTimeMillis()).getTime();
 
     @Value("${integration.marvel.public-key}")
@@ -14,4 +24,19 @@ public class MarvelApiConfig {
 
     @Value("${integration.marvel.private-key}")
     private String privateKey;
+
+    private String getHash() {
+        String hashDecoded = Long.toString(timestamp).concat(privateKey).concat(publicKey);
+        return md5Encoder.encode(hashDecoded);
+    }
+
+    // Parametros para realizar la autenticacion a la api
+    public Map<String, String> getAuthenticationQueryParams() {
+        Map<String, String> securityQueryParams = new HashMap<>();
+        securityQueryParams.put("ts", Long.toString(timestamp));
+        securityQueryParams.put("apikey", publicKey);
+        securityQueryParams.put("hash", this.getHash());
+
+        return securityQueryParams;
+    }
 }
